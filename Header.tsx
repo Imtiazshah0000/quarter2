b10be2d63ab0a7d5e49681294eda8166e57756fe
Imtiazshah0000 <161019525+Imtiazshah0000@users.noreply.github.com>
2024-11-12 23:36:02 +0500

    Add files via upload

---
+++ Header.tsx
@@ -0,0 +1,20 @@
+function Header ()  {
+    return (
+      <div className="h-[134px] p-4 flex  " style={{backgroundColor:'rgb(162, 152 ,117) '}}>
+        <h1 className='w-[242.56px] h-[87.45px]font-bold text-white flex '>Purity</h1>
+        <div className=' flex gap-4 items-center justify-center'>
+          <input 
+          type="text"
+          placeholder='Search for Gold Jewellery,Diamond Jewellery and more...'
+          className='p-2 rounded-lg text-white-900 w-[769.89px] h-[56.24px]'
+         
+          />
+    </div>    
+          
+    </div>
+        
+      
+      )
+    }
+    
+    export default Header
